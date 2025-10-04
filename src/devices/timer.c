1f4e5aa2258128a26a5faf21553b44b1ff54fb02
Maksims Orlovs <m.orlov10@gmail.com>
2025-10-05 01:54:59 +0200

    wake in timer

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -173,6 +173,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
   thread_tick ();
+  thread_foreach(thread_wake, 0);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
