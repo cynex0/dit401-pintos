7d592db4c6592c2b362f2d55d40ed76595cbaaea
Maksims Orlovs <m.orlov10@gmail.com>
2025-10-09 13:46:09 +0200

    unblock before ticking the thread

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -180,8 +180,8 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
-  thread_tick ();
   thread_foreach (thread_wake, &ticks);
+  thread_tick ();
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
